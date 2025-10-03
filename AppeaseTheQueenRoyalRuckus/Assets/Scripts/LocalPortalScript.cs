5d7bfc64e4bbe0cfb4b511f4e009fb3145594d05
Richard Rattner <rrattner1@gmail.com>
2025-10-03 13:02:20 -0700

    Portals Fixed

---
+++ AppeaseTheQueenRoyalRuckus/Assets/Scripts/LocalPortalScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPortalScript : MonoBehaviour
+{
+    [SerializeField] private GameObject exitPortal;
+    [SerializeField] private float portalDeactivationTimer;
+    public bool isTestPortal;
+    private Rigidbody2D pinballRigidbody;
+
+    private float exitPortalInactiveTime;
+
+    void Start()
+    {
+        exitPortalInactiveTime = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /**if (exitPortalInactiveTime < Time.time)
+        {
+            exitPortal.SetActive(true);
+        }
+        if (isTestPortal)
+        {
+            print("Current coordinates to move to are: x:"+exitPortal.transform.position.x+", y: "+exitPortal.transform.position.y+"\n");
+        }**/
+    }
+
+    void OnTriggerEnter2D(Collider2D pinballCollider)
+    {
+        if ((pinballCollider.gameObject.tag == "Pinball") && (exitPortalInactiveTime < Time.time))
+        {
+            print("Collision Detected.");
+            GameObject myPinball = pinballCollider.gameObject;
+            Transform newLocation = exitPortal.transform;
+
+            pinballRigidbody = myPinball.GetComponent<Rigidbody2D>();
+
+            exitPortal.GetComponent<LocalPortalScript>().portalInactive();
+            
+
+            //myPinball.transform.position = exitPortal.transform.position;
+            myPinball.transform.Translate(exitPortal.transform.position - myPinball.transform.position);
+        }
+    }
+    private void portalInactive()
+    {
+         exitPortalInactiveTime = Time.time + portalDeactivationTimer;
+    }
+}
